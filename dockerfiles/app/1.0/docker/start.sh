9044133db13faf72340bdd58cc8e27039592a51b
Ivan Bogomolov <hello@ivanbogomolov.ru>
2022-02-19 15:40:33 +0300

    Changes:
    - wget, tzdata in app/symfony image
    - docker-compose config enable TZ=Europe/Moscow
    - in php config date.timezone = Europe/Moscow and for dev config xdebug
      enabled
    - add short scripts to run base commands
    - add crontab update script and posibility to run crontab

--- dockerfiles/app/1.0/docker/start.sh
+++ dockerfiles/app/1.0/docker/start.sh
@@ -5,11 +5,6 @@ deluser app &> /dev/null
 # $APP_USER_UID must be your host user!
 adduser -h /home/app -u $APP_USER_UID -D app
 
-# In some cases dir doesn't exists
-mkdir -p /var/cache/nginx &> /dev/null
-# When host user UID go to container, this user needs access
-chown nginx:nginx -R /var/cache/nginx
-
 # This need for cron jobs /var/www/html path is important -> must be workdir in Dockerfile
 # cat /var/www/html/.crontab | crontab -u app -
 
